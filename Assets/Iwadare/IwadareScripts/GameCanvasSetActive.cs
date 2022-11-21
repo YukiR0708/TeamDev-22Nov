ac57975b35d2c9678dc23a6088e1331c52b6028e
DRGO <abokado0901@gmail.com>
2022-11-21 23:52:09 +0900

    ResultのCanvasの追加 ポーズ中にPowの数をださないなどの調整

---
+++ Assets/Iwadare/IwadareScripts/GameCanvasSetActive.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCanvasSetActive : MonoBehaviour
+{
+    GameObject _gameCanvas;
+
+    private void Awake()
+    {
+        _gameCanvas = GameObject.Find("GameCanvas");
+    }
+
+    private void OnEnable()
+    {
+        GameManager.Instance._playGame = false;
+        _gameCanvas.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        GameManager.Instance._playGame = true;
+        _gameCanvas.SetActive(true);
+    }
+}
--- Assets/Iwadare/IwadareScripts/RanRankScript.cs
+++ Assets/Iwadare/IwadareScripts/RanRankScript.cs
@@ -5,19 +5,28 @@ using UnityEngine.UI;
 
 public class RanRankScript : MonoBehaviour
 {
-    private Text _rank;
+    private Text _rankText;
+    private Text _countText;
+    private GameObject _gameCanvas;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-
-        _rank = GetComponent<Text>();
-        //最初false状態にしておいてtrueになったらrank表示。
-        _rank.text = GameManager.Instance.Rank();
+        //OnEnableはStartより早く動くのでAwakeでエラーを出さないようにする。
+        _countText = transform.GetChild(1).gameObject.GetComponent<Text>();
+        _rankText = transform.GetChild(2).gameObject.GetComponent<Text>();
+        _gameCanvas = GameObject.Find("GameCanvas");
+        gameObject.SetActive(false);
+    }
+    private void OnEnable()
+    {
+        //startに書くとうまく反映されないのでアクティブ時に反映させるようにした。
+        _rankText.text = GameManager.Instance.Rank();
+        GameManager.Instance.ShowText(_countText);
+        _gameCanvas.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        _gameCanvas.SetActive(true);
     }
 }
