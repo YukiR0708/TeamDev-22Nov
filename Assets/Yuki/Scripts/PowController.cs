eee1b37bf7e55d066b5dd37968a543728c42ba44
Ray <rei.piyo.55687@gmail.com>
2022-11-22 01:05:59 +0900

    [Update]敵のタグによって処理を分岐 [Fix]置いたPowブロックに別のPowブロックを投げると両方消えてしまう不具合を修正

--- Assets/Yuki/Scripts/PowController.cs
+++ Assets/Yuki/Scripts/PowController.cs
@@ -8,6 +8,15 @@ public class PowController : MonoBehaviour
     Rigidbody2D _powRb = default;
    [SerializeField, Header("Powを投げるインターバル")] float _throwInterval = default;
 
+    /// <summary> 各Powの役割識別  /// </summary>
+    private enum PowJudge
+    {
+        Thrown, //投げられたPow
+        Put　//置かれたPow
+    }
+
+    private PowJudge _powJudge;
+
     private void Awake()
     {
         _playerCon = GameObject.Find("Player").GetComponent<PlayerController>();
@@ -16,6 +25,11 @@ public class PowController : MonoBehaviour
         if (_playerCon._playMode == PlayerController.PowMode.Throw)  //投げモードだったら
         {
             gameObject.layer = LayerMask.NameToLayer("ThrownPow");  //Playerとぶつからないよう、自分のレイヤーを変える
+            _powJudge = PowJudge.Thrown;
+        }
+        else if(_playerCon._playMode == PlayerController.PowMode.Put)
+        {
+            _powJudge = PowJudge.Put;
         }
 
     }
@@ -48,16 +62,15 @@ public class PowController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (_playerCon._playMode == PlayerController.PowMode.Throw)
+        if (_playerCon._playMode == PlayerController.PowMode.Throw && _powJudge == PowJudge.Thrown)
         {
             StartCoroutine(ThrowCoroutine());
 
             if (other.gameObject.CompareTag("Ground") || other.gameObject.CompareTag("Pow")) //投げモードで地面か壁にあたったら
             {
                 Debug.Log("ザコだけしぬ");
-                //SE鳴らして画面を振動させる。
-                //画面内のザコ敵だけを取得して倒す
-
+                //SE鳴らして画面を振動させる。波を出す。
+                DestroyZako();
 
 
 
@@ -65,8 +78,15 @@ public class PowController : MonoBehaviour
             else if (other.gameObject.CompareTag("ShakeDown") || other.gameObject.CompareTag("HitDown")) //ザコか強敵にあたったら
             {
                 Debug.Log("当たった敵とザコがしぬ");
-                //SE鳴らして画面を振動させる。
-                //当たった敵と画面内のザコ敵が倒れる
+                //SE鳴らして画面を振動させる。波をだす。
+                Destroy(other.gameObject); //当たった敵と画面内のザコ敵が倒れる
+                DestroyZako();
+
+            }
+
+            else if(other.gameObject.CompareTag("Boss"))
+            {
+                Debug.Log("BossのHPが削れる");
             }
 
         }
@@ -80,4 +100,15 @@ public class PowController : MonoBehaviour
         Destroy(this.gameObject);
     }
 
+    /// <summary> ザコ敵だけを取得して消す。画面内外のタグ制御は敵側のスクリプトに記述 </summary>
+    void DestroyZako()
+    {
+        GameObject[] zakos = GameObject.FindGameObjectsWithTag("ShakeDown"); //
+        foreach (GameObject enemies in zakos)
+        {
+            Destroy(enemies);
+        }
+
+    }
 }
+
